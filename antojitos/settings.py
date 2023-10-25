d464ca1d598dca0f4eeb507ddb4800fd54d43661
bryan <smithelunico123@gmail.com>
2023-10-25 15:56:07 -0500

    tercero

--- antojitos/settings.py
+++ antojitos/settings.py
@@ -145,3 +145,10 @@ MEDIA_URL = '/public/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True  # Permitir todas las solicitudes desde cualquier origen
+# o
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",  # Agrega los dominios permitidos
+]
